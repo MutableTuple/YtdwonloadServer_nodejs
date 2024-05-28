806dde892121af7fa89819708be08589de561c4c
MutableTuple <mutabletuple39@gmail.com>
2024-05-28 10:14:39 +0530

    changing cors

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require("express");
+
 const ytdl = require("ytdl-core");
 const cors = require("cors");
 const app = express();
 const port = 3001;
 
-const cors = require("cors");
-
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+app.use(cors());
 
 app.use(express.json());
-
-app.use(cors(corsOptions));
 
 app.post("/download", async (req, res) => {
   const videoUrl = req.body.url;
