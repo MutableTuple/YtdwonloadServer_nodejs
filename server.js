21b286db62b3253274b25ef28cb588569483d158
MutableTuple <mutabletuple39@gmail.com>
2024-05-28 09:39:30 +0530

    adding the backend

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/download", async (req, res) => {
+  const videoUrl = req.body.url;
+
+  if (!ytdl.validateURL(videoUrl)) {
+    return res.status(400).send("Invalid URL");
+  }
+
+  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+  ytdl(videoUrl, {
+    format: "mp4",
+  }).pipe(res);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
